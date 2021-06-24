66091650b066d6b7c0ec82944dd563c81ee49c44
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-06-24 20:38:29 +0200

    Tags printed in show (admin)

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,10 +3,12 @@
 @section('content')
     <div class="container">
 
+        {{-- Categories --}}
         @if($post_category)
         <div class="mt-2 mb-2">Categoria: {{ $post_category->name }}</div>
         @endif
 
+        {{-- Info --}}
         <h1>{{$post->title}}</h1>
 
         <h6>
@@ -19,6 +21,16 @@
         <p>
             {{$post->content}}
         </p>
+
+        {{-- Tags --}}
+        @if($post_tags)
+        <div class="mt-2 mb-2">
+            <strong>Tag:</strong> 
+            @foreach ($post_tags as $tag)
+                {{ $tag->name }}{{$loop->last ? '' : ', '}}
+            @endforeach    
+        </div>
+        @endif
 
         {{-- Edit --}}
         <a href="{{ route('admin.posts.edit', ['post' => $post->id ]) }}" class="btn btn-secondary">
