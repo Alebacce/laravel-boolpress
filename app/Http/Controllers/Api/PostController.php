1a687dd284021a691f07e0223a6faec6046fee40
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-06-25 17:28:18 +0200

    Json of posts created

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Fakepost;
 
 class PostController extends Controller
 {
     public function index()
     {
-        dd('Ciao');
+        $posts = Fakepost::all();
+
+        $result = [
+            'posts' => $posts,
+            'success' => true
+        ];
+
+        return response()->json($result);
     }
 }
