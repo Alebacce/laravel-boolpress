d1e61689f317de5ad011d431a0ab7aecb87d38eb
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-06-26 17:52:12 +0200

    Posts printed with vue

--- resources/js/posts.js
+++ resources/js/posts.js
@@ -1,7 +1,18 @@
+const { default: Axios } = require("axios");
+
 var app = new Vue({
     el: '#root',
 
     data: {
-        title: 'Lista post'
-    }
+        title: 'Lista post',
+        posts: []
+    },
+
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/posts-api')
+            .then(result => {
+                console.log(result.data);
+                this.posts = result.data;
+            })
+    } 
 });
--- resources/views/guest/posts/vue-posts.blade.php
+++ resources/views/guest/posts/vue-posts.blade.php
@@ -14,8 +14,30 @@
         <h1>Printed with Vue.js</h1>
         <div id="root">
             <h2>@{{ title }}</h2>
+
+            <div class="row">
+                <div v-for="post in posts" class="col-6 vue-posts">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">@{{ post.title }}</h5>
+                            <h6 class="card-title">@{{ post.author }}</h6>
+                            <p class="card-text">@{{ post.content }}</p>
+
+                            <div v-if="post.tags.length > 0">
+                                Tags:
+                            <ul>
+                                <li v-for="tag in post.tags">
+                                    @{{ tag.name }}
+                                </li>
+                            </ul>
+                            </div>
+                            {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
-        
     </div>    
 @endsection
 
